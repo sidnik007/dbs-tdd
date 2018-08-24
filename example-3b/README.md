a86872c9a78ca67a41f151a113c7fd021a279ab1
siddhesh.nikude <siddhesh.nikude@nelkinda.com>
2018-08-24 20:35:35 +0530

    Fixing exercise-3b.

---
+++ example-3b/README.md
@@ -0,0 +1,1 @@
+THis is an exercise. Look into the test files and follow the TODO comments
--- example-3b/src/test/java/factorial/com/dbs/FactorialTest.java
+++ example-3b/src/test/java/factorial/com/dbs/FactorialTest.java
@@ -1,5 +1,32 @@
 package factorial.com.dbs;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FactorialTest {
+	@Test
+	public void factorialOf_0_is_1() {
+		// Test that the factorial of 0 is 1.
+		int expected = 1;
+		int actual = Factorial.factorial(0);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void factorialOf_2_is_2() {
+		// Test that the factorial of 2 is 2.
+		int expected = 2;
+		int actual = Factorial.factorial(2);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void factorialOf_3_is_6() {
+		// Test that the factorial of 3 is 6.
+		int expected = 6;
+		int actual = Factorial.factorial(3);
+		assertEquals(expected, actual);
+	}
 
 }
